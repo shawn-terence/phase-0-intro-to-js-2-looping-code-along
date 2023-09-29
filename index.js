a8c5489a41e8e1c80fb3748b43081fd2a95842c5
terence <terenceshawn18@outlook.com>
2023-09-29 11:19:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 // Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    
-  }
-
-  return gifts;
-}
-
-wrapGifts(gifts);
 
 const names=["Charlie", "Samip", "Ali"];
 const event=["suprise"]
